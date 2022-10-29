c2f6e81f533f6be219660c7b4cea96211fb6b6c0
Umid Muzrapov <63548446+UmidMuzrapov@users.noreply.github.com>
2022-10-29 12:24:26 -0700

    Add files via upload

---
+++ UmidMuzrapov/Client/Pages/project/Project.cs
@@ -0,0 +1,13 @@
+﻿namespace UmidMuzrapov.Client.Pages.project
+{
+    public class Project
+    {
+        public int? Id { get; set; }
+        public string? Image { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public bool? LiveTest { get; set; }
+        public string? Tools { get; set; }
+
+    }
+}
