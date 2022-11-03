d4b6ca458be4a8dfd32ba0d1b90607428d1d98ab
Umid Muzrapov <63548446+UmidMuzrapov@users.noreply.github.com>
2022-11-02 23:42:11 -0700

    Add files via upload

---
+++ UmidMuzrapov/Client/Pages/Course.cs
@@ -0,0 +1,10 @@
+﻿namespace UmidMuzrapov.Client.Pages
+{
+    public class Course
+    {
+        public string? Name { get; set;}
+        public string? Grade { get; set;}
+        public string? Description { get; set;}
+
+    }
+}
