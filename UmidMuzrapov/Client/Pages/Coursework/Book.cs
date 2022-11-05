07cbb269066136bb8b4d8b1e950406a95d2c7997
Umid Muzrapov <63548446+UmidMuzrapov@users.noreply.github.com>
2022-11-05 00:24:54 -0700

    Add files via upload

---
+++ UmidMuzrapov/Client/Pages/Coursework/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace UmidMuzrapov.Client.Pages.Coursework
+{
+    public class Book
+    {
+        public string? Name { get; set; }
+
+        public string? Author { get; set; }
+    }
+}
---
+++ UmidMuzrapov/Client/Pages/Coursework/OnlineCourse.cs
@@ -0,0 +1,8 @@
+﻿namespace UmidMuzrapov.Client.Pages.Coursework
+{
+    public class OnlineCourse
+    {
+        public string? Name { get; set; }
+        public string? Platform { get; set; }
+    }
+}
