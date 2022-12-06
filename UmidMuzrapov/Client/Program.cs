08bb925d7bfa309d05233663749590abcba40a33
Umid Muzrapov <63548446+UmidMuzrapov@users.noreply.github.com>
2022-12-06 10:28:07 -0700

    Update Program.cs

--- UmidMuzrapov/Client/Program.cs
+++ UmidMuzrapov/Client/Program.cs
@@ -13,7 +13,33 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped<WeatherForecastService>();
+builder.Services.AddResponseCaching();
 builder.Services.AddTelerikBlazor();
+
+builder.Services.AddResponseCaching(options =>
+{
+    options.MaximumBodySize = 1024;
+    options.UseCaseSensitivePaths = true;
+});
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.UseResponseCaching();
+
+app.Use(async (context, next) =>
+{
+    context.Response.GetTypedHeaders().CacheControl =
+        new Microsoft.Net.Http.Headers.CacheControlHeaderValue()
+        {
+            Public = true,
+            MaxAge = TimeSpan.FromSeconds(20)
+        };
+    context.Response.Headers[Microsoft.Net.Http.Headers.HeaderNames.Vary] =
+        new string[] { "Accept-Encoding" };
+
+    await next();
+});
 
 await builder.Build().RunAsync();
