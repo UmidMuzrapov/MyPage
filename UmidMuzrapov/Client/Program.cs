dbe9471258c72e0ff563a24727c77cb0de130f75
Umid Muzrapov <63548446+UmidMuzrapov@users.noreply.github.com>
2022-12-06 10:45:07 -0700

    Update Program.cs

--- UmidMuzrapov/Client/Program.cs
+++ UmidMuzrapov/Client/Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -9,12 +10,30 @@ using UmidMuzrapov.Client;
 using UmidMuzrapov.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.Services.AddResponseCaching();
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddTelerikBlazor();
 
+app.UseHttpsRedirection();
+app.UseResponseCaching();
+
+app.Use(async (context, next) =>
+{
+    context.Response.GetTypedHeaders().CacheControl =
+        new Microsoft.Net.Http.Headers.CacheControlHeaderValue()
+        {
+            Public = true,
+            MaxAge = TimeSpan.FromSeconds(10)
+        };
+    context.Response.Headers[Microsoft.Net.Http.Headers.HeaderNames.Vary] =
+        new string[] { "Accept-Encoding" };
+
+    await next();
+});
 
 
 await builder.Build().RunAsync();
