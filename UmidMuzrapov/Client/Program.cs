a861a3b681a3d604fc0ba5fab54f2bf0849a5f23
Umid Muzrapov <63548446+UmidMuzrapov@users.noreply.github.com>
2022-12-15 20:15:31 -0700

    Update Program.cs

--- UmidMuzrapov/Client/Program.cs
+++ UmidMuzrapov/Client/Program.cs
@@ -6,6 +6,7 @@ using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 using UmidMuzrapov.Client;
+using TelerikBlazorApp1.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
@@ -13,6 +14,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<WeatherForecastService>();
 builder.Services.AddTelerikBlazor();
 
 await builder.Build().RunAsync();
